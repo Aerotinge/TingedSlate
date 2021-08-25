7eea7844237a8de0d1c1a2a65eca9810c605e788
Sergei Ivanov <Aerotinge@users.noreply.github.com>
2021-08-25 13:52:12 +0800

    flavours modification, imported from various private mini mod

---
+++ common/defines/01_custom_def.lua
@@ -0,0 +1,5 @@
+-- Format for overwriting define values:
+--
+NDefines.NDiplomacy.MAX_DIPLO_DISTANCE = 600						-- Most diplomacy is disallowed if two rulers are too distant
+NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION_GROUP = 100 	-- Bonus to max diplo distance for being of the same religion group
+NDefines.NFrontend.MAX_ZOOM_LEVEL = 3000
