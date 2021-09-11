c9bb3e71289f1f53b81cb62aaad581c2bd437afc
Sergei Ivanov <Aerotinge@users.noreply.github.com>
2021-09-11 16:59:37 +0800

    defines: retinue cap params much lower, empires of 1337 start have ~3k instead of 20k+ while empires of 1066 start have around 1k events: clean elector in offmap screen if the elector was elected as the Emperor.

--- common/defines/01_custom_def.lua
+++ common/defines/01_custom_def.lua
@@ -3,3 +3,5 @@
 NDefines.NDiplomacy.MAX_DIPLO_DISTANCE = 600						-- Most diplomacy is disallowed if two rulers are too distant
 NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION_GROUP = 100 	-- Bonus to max diplo distance for being of the same religion group
 NDefines.NFrontend.MAX_ZOOM_LEVEL = 3000
+NDefines.NMilitary.RETINUE_INCREASE_PER_TECH = 0.35
+NDefines.NMilitary.RETINUE_FROM_REALMSIZE = 0.8
