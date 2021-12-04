90facf3b55b32fc538efaec819bb020199ffa547
Sergei Ivanov <Aerotinge@users.noreply.github.com>
2021-12-04 13:49:03 +0800

    history: Seljuk 1066, roughly defines: vassal limit now a arithmetic progression of 6, 14, 22 instead of vanilla 10, 20, 30 laws: default value of centralization is now 2(Medium centralization)

--- common/defines/01_custom_def.lua
+++ common/defines/01_custom_def.lua
@@ -1,5 +1,8 @@
 -- Format for overwriting define values:
 --
+NDefines.NDiplomacy.VASSAL_LIMIT_DUKE_MULT = 6.0					-- Extra Vassal Limit from the ruler's rank
+NDefines.NDiplomacy.VASSAL_LIMIT_KING_MULT = 14.0					-- Extra Vassal Limit from the ruler's rank
+NDefines.NDiplomacy.VASSAL_LIMIT_EMPEROR_MULT = 24.0				-- Extra Vassal Limit from the ruler's rank
 NDefines.NDiplomacy.MAX_DIPLO_DISTANCE = 600						-- Most diplomacy is disallowed if two rulers are too distant
 NDefines.NDiplomacy.MAX_DIPLO_DISTANCE_SAME_RELIGION_GROUP = 100 	-- Bonus to max diplo distance for being of the same religion group
 NDefines.NFrontend.MAX_ZOOM_LEVEL = 3000
